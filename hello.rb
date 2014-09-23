0d88e891759174c59af5ba2fc8cdfde1c9a341c1
Numata <t.numata.3632@gmail.com>
2014-09-23 06:36:01 +0000

    Added route pattern /hello/:name

--- hello.rb
+++ hello.rb
@@ -8,3 +8,8 @@ get '/aiit' do
   "Hello aiit."
 end
 
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params[:name] is 'foo' or 'bar'
+  "Hello #{params[:name]}!"
+end
